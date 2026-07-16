b6e5febfeb669d34f1a5593183becae197348b42
Levi Marin Hernandez <levimh.makingsoftware@gmail.com>
2026-07-16 14:30:09 -0600

    Tipos de datos Numericos en Javascript

---
+++ Introduccion-JavaScript/IntroduccionJS/js/05.js
@@ -0,0 +1,35 @@
+/* Numericos */
+
+const nombre = "Levi";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+const numero6 = 6;
+const numero7 = 7;
+
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+console.log(typeof nombre);// string
+console.log(typeof numero1);// number
+console.log(typeof numero2);// number
+console.log(typeof numero3);// number
+console.log(typeof numero4);// number
+console.log(typeof numero5);// number
+
+// Operaciones
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
+console.log(numero7 % numero6); // Modulo o residuo de la divicion lo que queda de la divicion
+
